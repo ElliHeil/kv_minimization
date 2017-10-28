8395f4168a5f5511d23f582bee15f49e5a2bd4c3
Mikhail Ivchenko <ematirov@gmail.com>
2017-10-28 20:44:32 +0400

    Initial commit

---
+++ Minimization/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Minimization
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
