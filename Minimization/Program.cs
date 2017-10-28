456a108b1f5fc4546e1652e0e8b4968c25c84d17
Mikhail Ivchenko <ematirov@gmail.com>
2017-10-28 21:01:05 +0400

    Parse input and output function matrix

--- Minimization/Program.cs
+++ Minimization/Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 
 namespace Minimization
 {
-    internal class Program
+    internal static class Program
     {
         public static void Main(string[] args)
         {
-            
+            var n = int.Parse(Console.ReadLine());
+            var values = Console.ReadLine().Select(x => int.Parse(x.ToString())).ToArray();
+
+            for (int i = 0; i < Math.Pow(2, n); ++i)
+            {
+                Console.WriteLine(Convert.ToString(i, 2).PadLeft(n, '0') + " = " + values[i]);
+            }
         }
     }
 }
